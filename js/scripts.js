247a4676f89420617dd69ad21ee3b643f799ea27
vitala072 <vitalii>
2018-08-22 22:32:54 +0200

    dodano calcul

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,38 @@
 'use strict';
-const res = document.querySelector('#res');
-function calc(e) {
-const inputValue = e.value;
+// // const res = document.querySelector('#res');
+// function calc(e) {
+// // const inputValue = e.value;
+// // if(inputValue == "C") {
+// //     res.value = '';
+// // }
+// // else if(inputValue == "=") {
+// //     res.value = eval(res.value);
+// // }else {
+// //     res.value += inputValue;
+// // }
+// };
 
-if(inputValue == "C") {
-    res.value = '';
-}
-else if(inputValue == "=") {
-    res.value = eval(res.value);
-}else {
-    res.value += inputValue;
-}};
+function calc(){}
+(function(){
+    function calcul(result, type){
+        this.res = document.querySelector('#' + result);
+    this.button = document.querySelectorAll('input[type="'+type+'"]');
+    this.calcRes();
+    }
+    calcul.prototype.calcRes = function(){
+        this.button.forEach(function(item){
+            item.addEventListener('click', function(e) {
+            e.preventDefault();
+                const inputValue = item.value;
+                if(inputValue == "C") {
+                    res.value = '';
+                }
+                else if(inputValue == "=") {
+                    res.value = eval(res.value);
+                }else {
+                    res.value += inputValue;
+                }}.bind(this))
+        });  
+    }
+    new calcul('res', 'button');
+    })()
